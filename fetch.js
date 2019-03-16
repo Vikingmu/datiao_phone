6ee50a316fc34e140f4dcfebfb3640cc3af50a69
vik <1020043186@qq.com>
2019-03-16 17:04:48 +0800

    null

--- fetch.js
+++ fetch.js
@@ -1,5 +1,5 @@
 let api = axios.create({
-    baseURL: 'https://1.cust.edu.cn/quickauth/',
+    baseURL: 'https://exam.m.cust.edu.cn/quickauth/',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
